6370333d43e60186c86c1446ebe93f9e2efd4daf
MHEN2606 <matthew.mahendra@gmail.com>
2023-11-12 01:01:42 +0700

    feat: Updated KNN Model
    Co-authored-by: Henry Anand <henryanandsr@users.noreply.github.com>

--- src/models/KNN.py
+++ src/models/KNN.py
@@ -1,21 +1,43 @@
+import numpy as np
+from collections import Counter
+
 class KNN_Model():
     # Constructor
     # Define the number of neighbors to use in prediction
     # Default neighbors = 5
-    def __init__(self, k: int = 5):
+    def __init__(self, k=3, dist=1):
         self.k = k
-
+        self.dist = dist
+    
     # Fit Method
     # Fit the training dataset to the model
     # X are the features, Y is the target
     def fit(self, X, y):
-        self.features= X.columns
-        self.target = y.to_numpy()
-        self.data = X.to_numpy()
-
+        self.features = X.columns
+        self.X_train = X.to_numpy()
+        self.y_train = y.to_numpy()
+    
     # Predict Method
     # Prediction using KNN Algorithm
     # X is the data to be predicted
     # Return Predicted Data
     def predict(self, X):
-        return self
+        result = [self.predict_el(x) for x in X.to_numpy()]
+        return result
+    
+    # Distance Method for KNN
+    def __euclidean_distance(self,p1,p2):
+        p1_numeric = np.array(p1, dtype=float)
+        p2_numeric = np.array(p2, dtype=float)
+        return np.sqrt(np.sum((p1_numeric - p2_numeric) ** 2))
+    
+    def predict_el(self, x):
+        if (self.dist == 1):
+            distances = [self.__euclidean_distance(x, x_train) for x_train in self.X_train]
+        else : 
+            # other distance e.g. : Manhattan
+            distances = [self.__euclidean_distance(x, x_train) for x_train in self.X_train]
+        k_indices = np.argsort(distances)[:self.k]
+        k_nearest_labels = [self.y_train[i] for i in k_indices]
+        most_common = Counter(k_nearest_labels).most_common()
+        return most_common[0][0]
